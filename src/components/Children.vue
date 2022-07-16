e862b1ce64f892f7645c5a04c66cbec82ae83fbf
Zoujiaxue <48728526+aoliao-Z@users.noreply.github.com>
2022-07-16 21:12:22 +0800

    Initial commit

---
+++ src/components/Children.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { defineProps, computed } from "vue";
+type ChildrenProps = {
+  name: string;
+};
+const props = defineProps<ChildrenProps>();
+const a = computed(() => {
+  return props.name + "!";
+});
+</script>
+
+<template>
+  <div>
+    {{ a }}
+  </div>
+</template>
---
+++ src/components/Parent.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import Children from "./Children.vue";
+import { ref } from "vue";
+const name = ref("a");
+setTimeout(() => {
+  name.value = "b";
+}, 1000);
+</script>
+
+<template>
+  <div>
+    <Children :name="name" />
+  </div>
+</template>
